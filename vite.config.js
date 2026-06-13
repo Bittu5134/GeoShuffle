83449c3b1dbd7a2abe927238eee1ddb969381188
Bittu5134 <ssr7a5134@gmail.com>
2026-06-13 10:43:15 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,9 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // Tell Vite that your source files live inside the src folder
+  base: "/GeoShuffle/",
+
   root: "src",
-
   plugins: [tailwindcss()],
-
-  // Tell Vite to output the final build folder back out in the main project root
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true, // Cleans up the old dist folder before rebuilding
-  },
+  build: { outDir: "../dist", emptyOutDir: true },
 });
